99d8e053acadc7b9585d4a1295a9270cb50cb6bd
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2024-01-03 16:59:02 +0200

    feat [server]: set up postgres db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,15 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import router from "./routes";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api", router);
 
 app.listen(PORT, () => {
 	console.log(`Server has started on port ${PORT}`);
