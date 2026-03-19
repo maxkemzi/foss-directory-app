9ad4d43121412a859cfdd00ef5edd99100ad825c
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2026-03-19 17:12:56 +0200

    disable secure for nodemailer

--- server/src/services/lib/MailSender.ts
+++ server/src/services/lib/MailSender.ts
@@ -5,7 +5,7 @@ class MailSender {
 	static transporter = nodemailer.createTransport({
 		host: env.SMTP_HOST,
 		port: env.SMTP_PORT,
-		secure: env.HTTPS_ENABLED,
+		secure: false,
 		auth: {
 			user: env.SMTP_USER,
 			pass: env.SMTP_PASSWORD
